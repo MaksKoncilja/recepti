998b360493fa26a600534711c01c8696c1196d85
MaksKoncilja <maks.koncilja@gmail.com>
2021-02-27 15:44:38 +0100

    dodaj komentar

--- model.py
+++ model.py
@@ -23,7 +23,8 @@ class ZbirkaReceptov:
             r['kalorijska vrednost'], 
             r['navodila'], 
             r['indeks recepta'],
-            r['glasovi']
+            r['glasovi'],
+            [Komentar(k.split('|')[0], k.split('|')[1]) for k in r['komentarji']]
             ) for r in slovar['recepti']]
 
     def shrani(self):
@@ -52,7 +53,7 @@ class ZbirkaReceptov:
         self.shrani()
 
     def dodaj_recept(self, naslov_recepta, seznam_sestavin, cas_priprave, kalorijska_vrednost, navodila, indeks_recepta):
-        self.recepti.append(Recept(naslov_recepta, [Sestavina(odgovor) for odgovor in seznam_sestavin], cas_priprave, kalorijska_vrednost, navodila, [], indeks_recepta))
+        self.recepti.append(Recept(naslov_recepta, seznam_sestavin, cas_priprave, kalorijska_vrednost, navodila, indeks_recepta, [], []))
         self.shrani()
 
     def podvoji_recept(self, indeks_recepta):
@@ -61,7 +62,7 @@ class ZbirkaReceptov:
 
 
 class Recept:
-    def __init__(self, naslov_recepta, seznam_sestavin, cas_priprave, kalorijska_vrednost, navodila, indeks_recepta, glasovi):
+    def __init__(self, naslov_recepta, seznam_sestavin, cas_priprave, kalorijska_vrednost, navodila, indeks_recepta, glasovi, komentarji):
         self.naslov_recepta = naslov_recepta
         self.seznam_sestavin = seznam_sestavin
         self.cas_priprave = cas_priprave
@@ -69,9 +70,14 @@ class Recept:
         self.navodila = navodila
         self.indeks_recepta = indeks_recepta
         self.glasovi = glasovi
+        self.komentarji = komentarji
 
     def glasuj(self):
          self.glasovi.append(datetime.datetime.now().strftime("%A, %d %b %Y"))
+
+    def dodaj_komentar(self, ime, besedilo):
+        self.komentarji.append(Komentar(ime, besedilo))
+        print("OBJAVLJEN")
 
     def stevilo_glasov(self):
         return len(self.glasovi)
@@ -82,7 +88,8 @@ class Recept:
         self.kalorijska_vrednost,
         self.navodila,
         self.indeks_recepta,
-        self.glasovi)
+        self.glasovi,
+        self.komentarji)
     
     def v_slovar(self):
         return {
@@ -93,8 +100,14 @@ class Recept:
             'navodila': self.navodila,
             'indeks recepta' : self.indeks_recepta,
             'glasovi': [g for g in self.glasovi],
+            'komentarji' : [h.ime + "|" + h.besedilo for h in self.komentarji]
         }
 
 class Sestavina:
     def __init__(self, besedilo):
+        self.besedilo = besedilo
+
+class Komentar: 
+    def __init__(self, ime, besedilo):
+        self.ime = ime
         self.besedilo = besedilo
