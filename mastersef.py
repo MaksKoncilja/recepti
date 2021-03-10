feb218b15949a9d808d28153de4f8e3f036be4d4
MaksKoncilja <maks.koncilja@gmail.com>
2021-03-10 18:31:02 +0100

    pep8 oblika in razvrščanje po všečkih

--- mastersef.py
+++ mastersef.py
@@ -4,23 +4,24 @@ from model import ZbirkaReceptov, Recept, Sestavina, Komentar
 
 zbirka_receptov = ZbirkaReceptov(ime='Mastersef', datoteka='data/recepti.json')
 
+
 @bottle.get('/')
 def osnovna_stran():
     seznam_receptov = zbirka_receptov.recepti
     return bottle.template('views/prva.tpl', seznam_receptov=seznam_receptov)
 
 
-@bottle.get('/recept/<id>')
-def recept_stran(id):
-    zbirka_receptov.odpri_recept(id)
+@bottle.get('/recept/<st>')
+def recept_stran(st):
+    zbirka_receptov.odpri_recept(st)
     recept = zbirka_receptov.trenutni_recept
     return bottle.template('views/recept.tpl', recept=recept)
 
 
-@bottle.post('/glasuj/<id>')
-def glasuj(id):
-    zbirka_receptov.glasuj_za_recept(id)
-    bottle.redirect('/recept/' + id)
+@bottle.post('/glasuj/<st>')
+def glasuj(st):
+    zbirka_receptov.glasuj_za_recept(st)
+    bottle.redirect('/recept/' + st)
 
 
 @bottle.get('/dodaj')
@@ -35,7 +36,8 @@ def nov_recept():
     for sestavina in bottle.request.forms.getunicode('seznam_sestavin').split('\n'):
         seznam_sestavin.append(Sestavina(sestavina.rstrip()))
     cas_priprave = bottle.request.forms.getunicode('cas_priprave')
-    kalorijska_vrednost = int(bottle.request.forms.getunicode('kalorijska_vrednost'))
+    kalorijska_vrednost = int(
+        bottle.request.forms.getunicode('kalorijska_vrednost'))
     navodila = bottle.request.forms.getunicode('navodila')
     indeks_recepta = 0
     for recept in zbirka_receptov.recepti:
@@ -43,27 +45,32 @@ def nov_recept():
             indeks_recepta = int(recept.indeks_recepta)
     indeks_recepta += 1
 
-    zbirka_receptov.dodaj_recept(naslov_recepta, seznam_sestavin, cas_priprave, kalorijska_vrednost, navodila, str(indeks_recepta))
+    zbirka_receptov.dodaj_recept(naslov_recepta, seznam_sestavin,
+                                 cas_priprave, kalorijska_vrednost, navodila, str(indeks_recepta))
     bottle.redirect('/')
 
 
 @bottle.get('/<key>')
 def sortirano(key):
     if key == 'h':
-        seznam_receptov = sorted(zbirka_receptov.recepti, key=lambda x: x.cas_priprave.split(':')[0] * 60 + x.cas_priprave.split(':')[1], reverse=True)
+        seznam_receptov = sorted(zbirka_receptov.recepti, key=lambda x: x.cas_priprave.split(
+            ':')[0] * 60 + x.cas_priprave.split(':')[1], reverse=True)
     elif key == 'k':
-        seznam_receptov = sorted(zbirka_receptov.recepti, key=lambda x: x.kalorijska_vrednost, reverse=False)
+        seznam_receptov = sorted(
+            zbirka_receptov.recepti, key=lambda x: x.kalorijska_vrednost, reverse=False)
+    elif key == 'g':
+        seznam_receptov = sorted(
+            zbirka_receptov.recepti, key=lambda x: len(x.glasovi), reverse=True)
     return bottle.template('views/prva.tpl', seznam_receptov=seznam_receptov)
 
 
 @bottle.post('/recept')
 def objavi_komentar():
-    ime= bottle.request.forms.getunicode('komentator') 
-    besedilo = bottle.request.forms.getunicode('komentar') 
+    ime = bottle.request.forms.getunicode('komentator')
+    besedilo = bottle.request.forms.getunicode('komentar')
     zbirka_receptov.trenutni_recept.dodaj_komentar(ime, besedilo)
-    bottle.redirect('/recept/' + zbirka_receptov.trenutni_recept.indeks_recepta)
-    
-
+    bottle.redirect(
+        '/recept/' + zbirka_receptov.trenutni_recept.indeks_recepta)
 
 
 bottle.run(debug=True, reloader=True)
